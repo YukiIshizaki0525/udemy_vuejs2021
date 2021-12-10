d3d91ffa396bf12348d41e3510c6cc7d4c2148ee
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-12-10 22:43:19 +0900

    section6完了

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+// vueファイルはimportしたらcomponentのオブジェクトになる
+
+import LikeNumber from './components/LikeNumber.vue'
 
 // Tipsの表示可否について
 Vue.config.productionTip = false
+Vue.component('LikeNumber', LikeNumber);
+
+// console.log(App);
 
 new Vue({
   render: h => h(App),
