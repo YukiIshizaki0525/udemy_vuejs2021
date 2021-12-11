bc1eff3dc727f94506acfa74a208d1b22a0858a9
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-12-11 16:02:14 +0900

    7-92完了

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <LikeHeader></LikeHeader>
+    <p>{{ number }}</p>
     <LikeNumber></LikeNumber>
+    <LikeNumber :total-number="number"></LikeNumber>
   </div>
 </template>
 
@@ -9,6 +11,11 @@
 import LikeHeader from "./components/LikeHeader.vue";
 
 export default{
+  data(){
+    return {
+      number: 14
+    }
+  },
   components:{
     LikeHeader
   }
