0a8bf7203b2f0fa68ba28617ed030ee22602f644
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-12-15 22:13:18 +0900

    9-123完了

--- src/App.vue
+++ src/App.vue
@@ -14,10 +14,13 @@
       <h2>イベントのフォーム</h2>
         <label for="title">タイトル</label>
         <input id="title" type="text" v-model.lazy="eventData.title">
-        <p>{{ eventData.title }}</p>
+        <pre>{{ eventData.title }}</pre>
         <label for="maxNumber">最大人数</label>
-      <input id="maxNumber" type="number" v-model.number="eventData.maxNumber">
-      <p>{{ eventData.maxNumber }}</p>
+        <input id="maxNumber" type="number" v-model.number="eventData.maxNumber">
+        <p>{{ eventData.maxNumber }}</p>
+        <label for="host">主催者</label>
+        <input id="host" type="text" v-model.trim="eventData.host">
+        <p>{{ eventData.host }}</p>
     </div>
   </div>
 </template>
@@ -34,7 +37,8 @@ export default{
       currentComponent: "Home",
       eventData: {
         title: "タイトル",
-        maxNumber: 0
+        maxNumber: 0,
+        host: "host"
       }
     }
   },
