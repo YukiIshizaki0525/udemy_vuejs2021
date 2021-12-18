a1161bcdd3041f765973592a9f088242e3ee3c23
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-12-18 19:55:08 +0900

    10-140完了

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,4 @@
 <template>
-  <p v-border="{width: '3px', color: 'red'}">Home</p>
+  <!-- // 複数の場合はオブジェクトで指定 -->
+  <p v-border:solid.round.shadow="{width: '3px', color: 'red'}">Home</p>
 </template>
--- src/main.js
+++ src/main.js
@@ -10,11 +10,20 @@ Vue.component('LikeNumber', LikeNumber);
 
 // カスタムディレクティブ(グローバル)
 Vue.directive("border", function(el, binding) {
-  el.style.border = "solid black 2px";
-
-  // カスタムディレクティブからデータを渡す
+  // カスタムディレクティブからデータを渡す=>binding
   el.style.borderWidth = binding.value.width;
   el.style.borderColor = binding.value.color;
+
+  // カスタムディレクティブに引数を渡す
+  el.style.borderStyle = binding.arg;
+
+  // カスタムディレクティブに修飾子を使う
+  if (binding.modifiers.round) {
+    el.style.borderRadius = '0.5rem';
+  }
+  if (binding.modifiers.shadow) {
+    el.style.boxShadow = '0 2px 5px rgba(0,0,0.26)';
+  }
 });
 
 
