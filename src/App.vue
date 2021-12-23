79adb56fb4429333f0f2317cb20c2aef462f6502
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-12-23 22:25:24 +0900

    12-158完了

--- src/App.vue
+++ src/App.vue
@@ -1,105 +1,44 @@
 <template>
-  <div>
-    <LikeHeader>
-      <h3>初めまして</h3>
-    </LikeHeader>
-
-    <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
-    <button @click="currentComponent = 'Home'">Home</button>
-    <button @click="currentComponent = 'About'">About</button>
-    <keep-alive>
-      <component :is="currentComponent"></component>
-    </keep-alive>
-    <div style="padding: 10rem;">
-      <h2>イベントのフォーム</h2>
-        <EventTitle v-model="eventData.title"></EventTitle>
-        <!-- <EventTitle :value="eventData.title"  @input="eventData.title = $event"></EventTitle> -->
-
-
-        <!-- <label for="title">タイトル</label>
-        <input id="title" type="text" v-model.lazy="eventData.title">
-        <pre>{{ eventData.title }}</pre> -->
-
-        <!-- v-model別の書き方 -->
-        <!-- <label for="title">タイトル</label>
-        <input id="title" type="text" :value="eventData.title"  @input="eventData.title = $event.target.value">
-        <pre>{{ eventData.title }}</pre> -->
-
-        <label for="maxNumber">最大人数</label>
-        <input id="maxNumber" type="number" v-model.number="eventData.maxNumber">
-        <p>{{ eventData.maxNumber }}</p>
-
-        <label for="host">主催者</label>
-        <input id="host" type="text" v-model.trim="eventData.host">
-        <p>{{ eventData.host }}</p>
-
-        <label for="detail">イベントの内容</label>
-        <textarea id="detail" cols="30" rows="10" type="text" v-model.trim="eventData.detail"></textarea>
-        <p style="white-space: pre">{{ eventData.detail }}</p>
-
-        <label for="isPrivate">非公開</label>
-        <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate">
-        <p>{{ eventData.isPrivate }}</p>
-
-        <p>参加条件</p>
-        <input type="checkbox" id="10" value="10代" v-model="eventData.target">
-        <label for="10">10代</label>
-
-        <input type="checkbox" id="20" value="20代" v-model="eventData.target">
-        <label for="20">20代</label>
-
-        <input type="checkbox" id="30" value="30代" v-model="eventData.target">
-        <label for="30">30代</label>
-        <p>{{ eventData.target }}</p>
-
-        <input type="radio" id="free" value="無料" v-model="eventData.price">
-        <label for="free">無料</label>
-        <input type="radio" id="paid" value="有料" v-model="eventData.price">
-        <label for="paid">有料</label>
-
-        <p>開催場所</p>
-        <select v-model="eventData.location">
-          <option v-for="location in locations" :key="location">{{ location }}</option>
-        </select>
-        <p>{{ eventData.location }}</p>
-    </div>
+  <div class="main">
+    <button @click="show = !show">切り替え</button>
+    <transition name="fade">
+      <p v-if="show">hello</p>
+    </transition>
   </div>
 </template>
 
 <script>
-import LikeHeader from "./components/LikeHeader.vue";
-import About from "./components/About.vue";
-import Home from "./components/Home.vue";
-import EventTitle from "./components/EventTitle.vue";
-
-export default{
+export default {
   data(){
     return {
-      number: 14,
-      currentComponent: "Home",
-      locations: ["東京", "大阪", "名古屋"],
-      eventData: {
-        title: "",
-        maxNumber: 0,
-        host: "",
-        detail: "",
-        isPrivate: false,
-        target: [],
-        price: "無料",
-        location: []
-      }
-    }
-  },
-  components:{
-    LikeHeader,
-    About,
-    Home,
-    EventTitle
-  },
-  methods: {
-    incrementNumber(value){
-      this.number = value
+      show: true
     }
   }
 }
 </script>
+
+<style scoped>
+.fade-enter {
+
+}
+.fade-enter-active {
+
+}
+.fade-enter-to {
+
+}
+.fade-leave {
+
+}
+.fade-leave-active {
+
+}
+.fade-leave-to {
+  
+}
+.main {
+  width: 70%;
+  margin: auto;
+  padding-top: 5rem;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,37 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-// vueファイルはimportしたらcomponentのオブジェクトになる
 
-import LikeNumber from './components/LikeNumber.vue'
-
-// Tipsの表示可否について
 Vue.config.productionTip = false
-Vue.component('LikeNumber', LikeNumber);
-
-// カスタムディレクティブ(グローバル)
-// Vue.directive("border", function(el, binding) {
-//   // カスタムディレクティブからデータを渡す=>binding
-//   el.style.borderWidth = binding.value.width;
-//   el.style.borderColor = binding.value.color;
-
-//   // カスタムディレクティブに引数を渡す
-//   el.style.borderStyle = binding.arg;
-
-//   // カスタムディレクティブに修飾子を使う
-//   if (binding.modifiers.round) {
-//     el.style.borderRadius = '0.5rem';
-//   }
-//   if (binding.modifiers.shadow) {
-//     el.style.boxShadow = '0 2px 5px rgba(0,0,0.26)';
-//   }
-// });
-
-// フィルター機能(グローバル登録)
-Vue.filter("upperCase", function(value) {
-  return value.toUpperCase();
-});
-
-// console.log(App);
 
 new Vue({
   render: h => h(App),
